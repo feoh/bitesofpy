177958432cd0f92614847890931e4a259d2961a6
feoh <feoh@feoh.org>
2020-07-02 10:13:31 -0400

    submission Bite 207 @ codechalleng.es

---
+++ 207/cached_property.py
@@ -0,0 +1,29 @@
+from random import random
+from time import sleep
+from functools import lru_cache
+
+
+
+class Planet:
+    """the nicest little orb this side of Orion's Belt"""
+
+    GRAVITY_CONSTANT = 42
+    TEMPORAL_SHIFT = 0.12345
+    SOLAR_MASS_UNITS = 'M\N{SUN}'
+
+    def __init__(self, color):
+        self.color = color
+        self._mass = None
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({repr(self.color)})'
+
+    @property
+    @lru_cache(maxsize=None)
+    def mass(self):
+        scale_factor = random()
+        sleep(self.TEMPORAL_SHIFT)
+        self._mass = (f'{round(scale_factor * self.GRAVITY_CONSTANT, 4)} '
+                      f'{self.SOLAR_MASS_UNITS}')
+        return self._mass
+
