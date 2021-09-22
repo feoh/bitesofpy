93283efc059342bc87faa9ca9678ae56eb5f3cd5
feoh <feoh@feoh.org>
2021-09-22 18:29:25 -0400

    10 / 12 tests passing! Just two more to go!

--- 30/directors.py
+++ 30/directors.py
@@ -2,6 +2,7 @@ import csv
 import statistics
 from collections import defaultdict, namedtuple
 import os
+from typing import List, Tuple, Any
 from urllib.request import urlretrieve
 
 BASE_URL = 'https://bites-data.s3.us-east-2.amazonaws.com/'
@@ -18,6 +19,9 @@ MIN_YEAR = 1960
 
 Movie = namedtuple('Movie', 'title year score')
 
+def strip_unprintable(str):
+    clean = [character for character in str if character.isprintable()]
+    return "".join(clean)
 
 def get_movies_by_director():
     """Extracts all movies from csv and stores them in a dict,
@@ -29,8 +33,8 @@ def get_movies_by_director():
     movie_csv_dict_reader = csv.DictReader(mcsv)
 
     for movie_dict in movie_csv_dict_reader:
-        this_director = movie_dict['director_name']
-        this_movie: Movie = Movie(title=movie_dict['movie_title'], year=movie_dict['title_year'], score=movie_dict[
+        this_director = strip_unprintable(movie_dict['director_name'])
+        this_movie: Movie = Movie(title=strip_unprintable(movie_dict['movie_title']), year=movie_dict['title_year'], score=movie_dict[
             "imdb_score"])
         movies_by_director.setdefault(this_director, [])
         movies_by_director[this_director].append(this_movie)
@@ -41,7 +45,7 @@ def get_movies_by_director():
 def calc_mean_score(movies):
     """Helper method to calculate mean of list of Movie namedtuples,
        round the mean to 1 decimal place"""
-    scores = [movie.score for movie in movies]
+    scores = [float(movie.score) for movie in movies]
     unrounded_mean = statistics.fmean(scores)
     return round(unrounded_mean, 1)
 
@@ -51,6 +55,14 @@ def get_average_scores(directors):
        return a list of tuples (director, average_score) ordered by highest
        score in descending order. Only take directors into account
        with >= MIN_MOVIES"""
-    tl = [tuple(director,calc_mean_score(directors[director])) for director in directors]
-    return tl
+    director_scores: list[tuple[Any, float]] = []
 
+    for director in directors:
+        if len(directors[director]) < MIN_MOVIES:
+            continue
+
+        mean_score = calc_mean_score((directors[director]))
+        director_scores.append((director, mean_score))
+
+    director_scores.sort(key = lambda t: t[1], reverse=True)
+    return director_scores
