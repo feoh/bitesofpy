04bff76542c86458d31d2a4382250dfbdf805d0c
Chris Patti <feoh@feoh.org>
2021-09-28 06:58:48 -0400

    Initial cut. Still needs multiple hoidays per day to work :)

--- 97/holidays.py
+++ 97/holidays.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 import os
+from typing import Union
 from urllib.request import urlretrieve
 
-from bs4 import BeautifulSoup
-
+import bs4.element
+from bs4 import BeautifulSoup, ResultSet, Tag, NavigableString
+import html5lib
+from datetime import datetime
 
 # prep data
 tmp = os.getenv("TMP", "/tmp")
@@ -25,7 +28,20 @@ def get_us_bank_holidays(content=content):
        holiday table (css class = list-table), and return a dict of
        keys -> months and values -> list of bank holidays"""
 
-    soup = BeautifulSoup(holidays_html, 'html5lib')
-    country_table = soup.find('table', 'country-table')
-    tc = list(country_table.children)
-    holiday_rows = tc[3]
+    soup = BeautifulSoup(content, 'html5lib')
+    holiday_table: bs4.element.Tag = soup.find('table','list-table')
+    holiday_rows: ResultSet = holiday_table.find_all('tr', {'class': 'holiday'})
+    holiday_row: bs4.element.Tag
+    for holiday_row in holiday_rows:
+        holiday_tds: ResultSet = holiday_row.find_all('td')
+        # e.. 2018-01-01
+        date_str = holiday_tds[1].contents[1]['datetime']
+        (yyyy, mm, dd) = date_str.split('-')
+        holiday_name = holiday_tds[3].text.strip()
+        # tds have 2018-01-01 and another has "New Year's Day" - what we want.
+        holidays[dd].append(holiday_name)
+
+    return holidays
+
+
+
