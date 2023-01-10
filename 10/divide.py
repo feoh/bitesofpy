cc1a3962587072e12fc3b58de16a94532e1f7535
Chris Patti <feoh@feoh.org>
2023-01-09 19:40:15 -0500

    Solution

---
+++ 10/divide.py
@@ -0,0 +1,12 @@
+def positive_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+    except (ZeroDivisionError):
+        return 0
+    except (TypeError):
+        raise TypeError
+    
+    if result < 0:
+        raise (ValueError)
+    
+    return result
